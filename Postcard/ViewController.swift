c70c95d8b795fbb36373906089162ca77bb1873f
danilogga <danilogga@gmail.com>
2014-12-22 13:01:11 -0300

    adicionando label name

--- Postcard/ViewController.swift
+++ Postcard/ViewController.swift
@@ -11,8 +11,11 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var messageLabel: UILabel!
+    @IBOutlet weak var nameLabel: UILabel!
+
     @IBOutlet weak var enterNameTextField: UITextField!
     @IBOutlet weak var enterMessageTextField: UITextField!
+
     @IBOutlet weak var mailButton: UIButton!
     
     
@@ -27,14 +30,24 @@ class ViewController: UIViewController {
     }
 
     @IBAction func sendMailButtonPressed(sender: UIButton) {
-        // adicionando comentário pra testar o commit
+        // seta o label message
         messageLabel.hidden = false;
         messageLabel.text = enterMessageTextField.text;
         messageLabel.textColor = UIColor.redColor();
-        
+
+        // seta o label name
+        nameLabel.hidden = false;
+        nameLabel.text = enterNameTextField.text;
+        nameLabel.textColor = UIColor.blueColor();
+
+        // limpa os text fields
+        enterNameTextField.text = "";
+        enterNameTextField.resignFirstResponder();
+
         enterMessageTextField.text = "";
         enterMessageTextField.resignFirstResponder();
         
+        // altera o nome do botão
         mailButton.setTitle("Mail Sent", forState: UIControlState.Normal);
     }
 
